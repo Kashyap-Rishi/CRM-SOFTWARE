7a9b73df59ff11c65891014626b02dfbe65077f3
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-06-11 15:56:04 +0530

    Fixes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,14 @@ const connectToDatabase = require("./utils/db");
 const { PORT } = require("./config/config");
 const startConsumers = require("./utils/startConsumer");
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173','https://crm-x.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+  
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 

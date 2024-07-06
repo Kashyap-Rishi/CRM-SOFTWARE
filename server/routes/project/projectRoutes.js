1013e9d9ecb4ec0e465b09a92353dfd6e1b9f515
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-07-06 22:43:33 +0530

    Fixes

--- server/routes/project/projectRoutes.js
+++ server/routes/project/projectRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProject, fetchAllProjects, updateCompleted, fetchProjectById } = require("../../controllers/projects/ProjectCOntroller");
+const { createProject, fetchAllProjects, updateCompleted, fetchProjectById } = require("../../controllers/projects/ProjectController");
 const { addTaskToProject } = require("../../controllers/projects/TaskController");
 
 
@@ -10,6 +10,7 @@ router.post('/:projectId/tasks', addTaskToProject);
 router.put('/mark-completed/:projectId', updateCompleted);
 router.get("/fetch-project",fetchAllProjects);
 router.get("/:projectId",fetchProjectById);
+
 
 
 module.exports = router;
